a211484d986ea7ead7db34547fd4b05379942805
Xantorohara <xantorohara@gmail.com>
2016-02-09 11:07:35 +0400

    Update readme add  example

--- examples/Xanto_TM1637_Counter/Xanto_TM1637_Counter.ino
+++ examples/Xanto_TM1637_Counter/Xanto_TM1637_Counter.ino
@@ -18,6 +18,9 @@ const uint8_t TM1637_SEGMENTS[] = {
 
 XantoI2C i2c(PIN_DISPLAY_CLK, PIN_DISPLAY_DIO);
 
+uint32_t counter = 0;
+uint8_t digits[] = {0, 0, 0, 0};
+
 void convertNumberToDigits(uint16_t number, uint8_t digits[]) {
   digits[3] = number / 1000 % 10;
   digits[2] = number / 100 % 10;
@@ -31,61 +34,38 @@ void convertDigitsToSegments(uint8_t digits[]) {
   }
 }
 
-uint32_t i = 0;
-uint8_t digits[] = {0, 0, 0, 0};
-
-
 void displayNumber(uint16_t number, uint8_t display_point = 0) {
   convertNumberToDigits(number, digits);
   convertDigitsToSegments(digits);
-
-  i2c.doStartWriteAckStop(0x02);
 
   if (display_point) {
     digits[2] |= 0x01;
   }
 
   uint8_t data[] = {0x03, digits[3], digits[2], digits[1], digits[0]};
+
+  // Command1 from the datasheet
+  i2c.doStartWriteAckStop(0x02);
+
+  // Command2 from the datasheet
   i2c.doStartWriteAckStop(data, 5);
 
+  // Command3 from the datasheet
   i2c.doStartWriteAckStop(0xF1);
 }
 
 void setup() {
-  displayNumber(0);
-  delay(1000);
-  displayNumber(1);
-  delay(1000);
-  displayNumber(12);
-  delay(1000);
-  displayNumber(123);
-  delay(1000);
-  displayNumber(1234);
-  delay(1000);
-  displayNumber(2345);
-  delay(1000);
-  displayNumber(3456);
-  delay(1000);
-  displayNumber(4567);
-  delay(1000);
-  displayNumber(5678);
-  delay(1000);
-  displayNumber(6789);
-  delay(1000);
-  displayNumber(7890);
-  delay(1000);
-  displayNumber(8900);
-  delay(1000);
-  displayNumber(9000);
-  delay(1000);
-  displayNumber(0);
-  delay(1000);
-
+  // display all digits
+  for (uint8_t i = 0; i < 10; i++) {
+    displayNumber(i*1111);
+    delay(1000);    
+  }
 }
 
 void loop() {
-  i++;
-  displayNumber(i, i % 2);
-  delay(250);
+  counter++;
+  // display incremented counter, with a ":" symbol no each odd step
+  displayNumber(counter, counter % 2);
+  delay(500);
 }
 
